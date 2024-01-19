5e8437193cd27af47e756930701318b3ba18d134
reubengann <reubengann@gmail.com>
2024-01-19 14:48:05 -0500

    Move functionality to a class

--- Metadata.CLI/Program.cs
+++ Metadata.CLI/Program.cs
@@ -1,34 +1,3 @@
-﻿using Dapper;
-using MusicVideoJukebox.Core;
-using System.Data.SQLite;
+﻿using MusicVideoJukebox.Core;
 
-using (var refConnection = new SQLiteConnection(@"Data Source=C:\Users\reube\Downloads\songs.db"))
-using (var destConnection = new SQLiteConnection(@"Data Source=E:\Videos\Music Videos\On Media Center\meta.db"))
-{
-    refConnection.Open();
-    destConnection.Open();
-    await destConnection.ExecuteAsync(@"
-create table if not exists videos (
-video_id integer primary key autoincrement,
-filename text NOT NULL,
-""year"" integer NULL,
-title text NOT NULL, 
-album text NULL, 
-artist text NOT NULL
-)
-");
-    var fileNames = Directory.EnumerateFiles("E:\\Videos\\Music Videos\\On Media Center").Where(x => x.EndsWith(".mp4")).ToHashSet();
-    var existingRows = await destConnection.QueryAsync<VideoRow>("SELECT video_id, filename, \"year\", title, album, artist FROM videos");
-    var filesInDb = existingRows.Select(x => Path.Combine("E:\\Videos\\Music Videos\\On Media Center", x.filename)).ToHashSet();
-    var toRemove = filesInDb.Except(fileNames);
-    var toAdd = fileNames.Except(filesInDb);
-    // TODO: Remove items.
-    var agent = new LibraryFillInMetadataProvider(refConnection);
-    foreach (var row in toAdd)
-    {
-        var (artist, title) = FileNameHelpers.ParseFileNameIntoArtistTitle(row);
-        var metadata = await agent.GetMetadata(artist, title);
-        var vr = new VideoRow { artist = artist, title = title, album = metadata.Album, filename = Path.GetFileName(row), year = metadata.Year };
-        await destConnection.ExecuteAsync("INSERT INTO videos (filename, \"year\", title, album, artist) values (@filename, @year, @title, @album, @artist)", vr);
-    }
-}
+await BackfillVideoDatabaseBuilder.BuildAsync(@"C:\Users\reube\Downloads\songs.db", @"E:\Videos\Music Videos\On Media Center\meta.db");
