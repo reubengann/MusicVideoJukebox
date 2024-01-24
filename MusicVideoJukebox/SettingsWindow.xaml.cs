4d28b6a4b2617d895e584273accd84977558750d
reubengann <reubengann@gmail.com>
2024-01-24 16:10:58 -0500

    Close the settings window

--- MusicVideoJukebox/SettingsWindow.xaml.cs
+++ MusicVideoJukebox/SettingsWindow.xaml.cs
@@ -46,5 +46,19 @@ namespace MusicVideoJukebox
             }
             handleSelection = true;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (vm.HasSettingsToSave)
+            {
+                MessageBoxResult result = MessageBox.Show
+                        ("There are unsaved changes to the playlist. Discard?", "Discard changes?", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.No)
+                {
+                    return;
+                }
+            }
+            Close();
+        }
     }
 }
