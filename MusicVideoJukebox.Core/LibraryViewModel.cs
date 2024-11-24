3bb052a87b7afa6446bcdd7cd0e1f67e0941130c
Reuben Gann <reubengann@gmail.com>
2024-11-24 17:05:22 -0500

    Icons

--- MusicVideoJukebox.Core/LibraryViewModel.cs
+++ MusicVideoJukebox.Core/LibraryViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace MusicVideoJukebox.Core
+﻿using System.Collections.ObjectModel;
+
+namespace MusicVideoJukebox.Core
 {
+    public class LibraryItem
+    {
+        public string? Icon { get; set; }
+        required public string Name { get; set; }
+        public bool IsAddButton { get; set; } // Flag to differentiate "Add Library"
+    }
+
     public class LibraryViewModel : BaseViewModel
     {
+        public ObservableCollection<LibraryItem> Items { get; } = new ObservableCollection<LibraryItem>();
+
+        public LibraryViewModel()
+        {
+            Items.Add(new LibraryItem { Icon = "Images/library_music.svg", IsAddButton = false, Name = "Library 1" });
+            Items.Add(new LibraryItem { Icon = "/Images/library_add.svg", IsAddButton = true, Name = "Add new"});
+        }
     }
 }
--- MusicVideoJukebox/Views/LibrariesView.xaml.cs
+++ MusicVideoJukebox/Views/LibrariesView.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace MusicVideoJukebox.Views
 {
@@ -23,6 +10,16 @@ namespace MusicVideoJukebox.Views
         public LibrariesView()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            //if (sender is FrameworkElement element &&
+            //element.DataContext is ItemViewModel hoveredViewModel
+            //&& DataContext is LauncherViewModel launcherViewModel)
+            //{
+            //    launcherViewModel.Hovered(hoveredViewModel);
+            //}
         }
     }
 }
