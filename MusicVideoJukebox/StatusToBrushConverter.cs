d1acf6b8b1b77e3fecd39c945df41801e51ab437
Reuben Gann <reubengann@gmail.com>
2024-11-29 14:00:57 -0500

    Show status

---
+++ MusicVideoJukebox/StatusToBrushConverter.cs
@@ -0,0 +1,34 @@
+﻿using MusicVideoJukebox.Core.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MusicVideoJukebox
+{
+    public class StatusToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not MetadataStatus status) return Brushes.Gray;
+
+            return status switch
+            {
+                MetadataStatus.Fetching => Brushes.Yellow,
+                MetadataStatus.Done => Brushes.Green,
+                MetadataStatus.Manual => Brushes.Orange,
+                MetadataStatus.NotFound => Brushes.Red,
+                _ => Brushes.Gray
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MusicVideoJukebox/StatusToIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using Material.Icons;
+using MusicVideoJukebox.Core.Metadata;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MusicVideoJukebox
+{
+    public class StatusToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not MetadataStatus status)
+            {
+                return MaterialIconKind.HelpCircleOutline; // Default icon
+            }
+
+            return status switch
+            {
+                MetadataStatus.Fetching => MaterialIconKind.CloudSync,
+                MetadataStatus.Done => MaterialIconKind.Check,
+                MetadataStatus.Manual => MaterialIconKind.Pencil,
+                MetadataStatus.NotFound => MaterialIconKind.Alert,
+                _ => MaterialIconKind.HelpCircleOutline
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
