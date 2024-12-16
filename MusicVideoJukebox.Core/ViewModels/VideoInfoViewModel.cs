b5d71b4701b39170e3858cb83cbfa6e6e466f453
Reuben Gann <reubengann@gmail.com>
2024-12-15 23:44:53 -0500

    Move to file

---
+++ MusicVideoJukebox.Core/ViewModels/VideoInfoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MusicVideoJukebox.Core.Metadata;
+
+namespace MusicVideoJukebox.Core.ViewModels
+{
+    public class VideoInfoViewModel : BaseViewModel
+    {
+        private readonly PlaylistTrack playlistTrack;
+
+        public VideoInfoViewModel(PlaylistTrack playlistTrack)
+        {
+            this.playlistTrack = playlistTrack;
+        }
+
+        public string Artist => playlistTrack.Artist;
+        public string Title => $"\"{playlistTrack.Title}\"";
+        public string Album => playlistTrack.Album ?? "";
+        public string Year => playlistTrack.ReleaseYear?.ToString() ?? "";
+    }
+}
--- MusicVideoJukebox.Core/ViewModels/VideoPlayingViewModel.cs
+++ MusicVideoJukebox.Core/ViewModels/VideoPlayingViewModel.cs
@@ -194,19 +194,4 @@ namespace MusicVideoJukebox.Core.ViewModels
             Play();
         }
     }
-
-    public class VideoInfoViewModel : BaseViewModel
-    {
-        private readonly PlaylistTrack playlistTrack;
-
-        public VideoInfoViewModel(PlaylistTrack playlistTrack)
-        {
-            this.playlistTrack = playlistTrack;
-        }
-
-        public string Artist => playlistTrack.Artist;
-        public string Title => $"\"{playlistTrack.Title}\"";
-        public string Album => playlistTrack.Album ?? "";
-        public string Year => playlistTrack.ReleaseYear?.ToString() ?? "";
-    }
 }
