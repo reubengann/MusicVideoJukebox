5b855b57d3ad8a2471e25eb2777fa7d974a5a856
Reuben Gann <reubengann@gmail.com>
2024-12-19 15:18:34 -0500

    Fix margin when scaling

---
+++ MusicVideoJukebox/Converters/BoolToColumnConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MusicVideoJukebox
+{
+    public class BoolToColumnConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool shown)
+            {
+                return shown ? 2 : 1;
+            }
+
+            return 0; // Default fallback
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
