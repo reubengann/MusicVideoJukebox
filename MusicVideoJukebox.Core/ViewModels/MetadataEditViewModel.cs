d5ff60b5ab976a7bfd80b00a80cb440e3f61466b
Reuben Gann <reubengann@gmail.com>
2025-04-05 13:47:58 -0400

    Redesign

--- MusicVideoJukebox.Core/ViewModels/MetadataEditViewModel.cs
+++ MusicVideoJukebox.Core/ViewModels/MetadataEditViewModel.cs
@@ -3,38 +3,22 @@ using MusicVideoJukebox.Core.Metadata;
 using MusicVideoJukebox.Core.UserInterface;
 using Prism.Commands;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace MusicVideoJukebox.Core.ViewModels
 {
     public class MetadataEntryViewModel : BaseViewModel
     {
-        bool settingProgrammatically = false;
-
         public int VideoId { get => videoMetadata.VideoId; }
         public string Filename { get => videoMetadata.Filename; }
-        public string Title { get => videoMetadata.Title; set => SetPropertyForMetadata(videoMetadata.Title, value, v => videoMetadata.Title = v); }
-        public string Artist { get => videoMetadata.Artist; set => SetPropertyForMetadata(videoMetadata.Artist, value, v => videoMetadata.Artist = v); }
-        public string? Album { get => videoMetadata.Album; set => SetPropertyForMetadata(videoMetadata.Album, value, v => videoMetadata.Album = v); }
-        public int? Year { get => videoMetadata.ReleaseYear; set => SetPropertyForMetadata(videoMetadata.ReleaseYear, value, v => videoMetadata.ReleaseYear = v); }
+        public string Title { get => videoMetadata.Title; set => SetUnderlyingProperty(videoMetadata.Title, value, v => videoMetadata.Title = v); }
+        public string Artist { get => videoMetadata.Artist; set => SetUnderlyingProperty(videoMetadata.Artist, value, v => videoMetadata.Artist = v); }
+        public string? Album { get => videoMetadata.Album; set => SetUnderlyingProperty(videoMetadata.Album, value, v => videoMetadata.Album = v); }
+        public int? Year { get => videoMetadata.ReleaseYear; set => SetUnderlyingProperty(videoMetadata.ReleaseYear, value, v => videoMetadata.ReleaseYear = v); }
         public MetadataStatus Status
         {
             get => videoMetadata.Status;
-            set { _status = value; SetPropertyForMetadata(videoMetadata.Status, value, v => videoMetadata.Status = v); }
-        }
-
-        private bool SetPropertyForMetadata<T>(T currentValue, T newValue, Action<T> setValue, [CallerMemberName] string propertyName = null!)
-        {
-            if (EqualityComparer<T>.Default.Equals(currentValue, newValue))
-                return false;
-
-            setValue(newValue); // Update the value in the wrapped object
-            OnPropertyChanged(propertyName); // Notify UI
-            MaybeTouch();
-            return true;
+            set { videoMetadata.Status = value; SetUnderlyingProperty(videoMetadata.Status, value, v => videoMetadata.Status = v); }
         }
 
         public MetadataEntryViewModel(VideoMetadata videoMetadata)
@@ -42,31 +26,9 @@ namespace MusicVideoJukebox.Core.ViewModels
             this.videoMetadata = videoMetadata;
         }
 
-        private MetadataStatus _status = MetadataStatus.NotDone;
         private readonly VideoMetadata videoMetadata;
-        
-        private bool isModified = false;
-
-        public bool IsModified { get => isModified; private set => SetProperty(ref isModified, value); }
-        
-        public void StartProgrammaticUpdate() { settingProgrammatically = true; Status = MetadataStatus.Fetching; }
-        public void EndProgrammaticUpdate() { settingProgrammatically = false; }
-
-        private void MaybeTouch()
-        {
-            IsModified = true;
-            if (!settingProgrammatically)
-            {
-                Status = MetadataStatus.Manual;
-            }
-        }
 
         public VideoMetadata MetadataObject => videoMetadata;
-
-        public void Reset()
-        {
-            IsModified = false;
-        }
     }
 
     public class MetadataEditViewModel : AsyncInitializeableViewModel
@@ -75,10 +37,8 @@ namespace MusicVideoJukebox.Core.ViewModels
 
         public bool IsBusy { get => isBusy; set { isBusy = value; SetProperty(ref isBusy, value); } }
 
-        public ICommand FetchMetadataCommand { get; }
+        public ICommand EditMetadataCommand { get; }
         public ICommand RefreshDatabaseCommand { get; }
-        public DelegateCommand SaveChangesCommand { get; }
-        public ICommand LaunchMatchDialogCommand { get; }
 
         private readonly IMetadataManagerFactory metadataManagerFactory;
         private readonly LibraryStore libraryStore;
@@ -91,14 +51,11 @@ namespace MusicVideoJukebox.Core.ViewModels
 
         public MetadataEditViewModel(IMetadataManagerFactory metadataManagerFactory, LibraryStore libraryStore, IDialogService dialogService)
         {
-            FetchMetadataCommand = new DelegateCommand(LaunchMatchDialog);
+            EditMetadataCommand = new DelegateCommand(LaunchMatchDialog);
             RefreshDatabaseCommand = new DelegateCommand(async () => await ReconcileCurrentFolderContents());
-            SaveChangesCommand = CreateSafeAsyncCommand(SaveChanges, CanSaveChanges);
-            LaunchMatchDialogCommand = new DelegateCommand(LaunchMatchDialog);
             this.metadataManagerFactory = metadataManagerFactory;
             this.libraryStore = libraryStore;
             this.dialogService = dialogService;
-            MetadataEntries.CollectionChanged += MetadataEntries_CollectionChanged;
         }
 
         private void LaunchMatchDialog()
@@ -109,68 +66,11 @@ namespace MusicVideoJukebox.Core.ViewModels
             {
                 ArgumentNullException.ThrowIfNull(result.ScoredMetadata);
                 var scoredMetadata = result.ScoredMetadata;
-                SelectedItem.StartProgrammaticUpdate();
                 SelectedItem.Artist = scoredMetadata.Artist;
                 SelectedItem.Album = scoredMetadata.AlbumTitle;
                 SelectedItem.Title = scoredMetadata.Title;
                 SelectedItem.Year = scoredMetadata.ReleaseYear;
                 SelectedItem.Status = MetadataStatus.Done;
-                SelectedItem.EndProgrammaticUpdate();
-            }
-        }
-
-        private bool CanSaveChanges()
-        {
-            return MetadataEntries.Any(entry => entry.IsModified);
-        }
-
-        private void MetadataEntries_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.NewItems != null)
-            {
-                foreach (MetadataEntryViewModel newItem in e.NewItems)
-                {
-                    newItem.PropertyChanged += MetadataEntry_PropertyChanged;
-                }
-            }
-            if (e.OldItems != null)
-            {
-                foreach (MetadataEntryViewModel oldItem in e.OldItems)
-                {
-                    oldItem.PropertyChanged -= MetadataEntry_PropertyChanged;
-                }
-            }
-        }
-
-        private void MetadataEntry_PropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(MetadataEntryViewModel.IsModified))
-            {
-                SaveChangesCommand.RaiseCanExecuteChanged();
-            }
-        }
-
-        private async Task SaveChanges()
-        {
-            IsBusy = true;
-            try
-            {
-                foreach (var entry in MetadataEntries)
-                {
-                    if (entry.IsModified)
-                    {
-                        await metadataManager.VideoRepo.UpdateMetadata(entry.MetadataObject);
-                        entry.Reset();
-                    }
-                }
-            }
-            catch(Exception ex)
-            {
-                dialogService.ShowError($"Error: {ex.Message}");
-            }
-            finally
-            {
-                IsBusy = false;
             }
         }
 
@@ -186,7 +86,7 @@ namespace MusicVideoJukebox.Core.ViewModels
         public override async Task Initialize()
         {
             if (libraryStore.CurrentState.LibraryPath == null) return;
-            metadataManager =  metadataManagerFactory.Create(libraryStore.CurrentState.LibraryPath);
+            metadataManager = metadataManagerFactory.Create(libraryStore.CurrentState.LibraryPath);
             await LoadMetadata();
         }
 
@@ -196,26 +96,8 @@ namespace MusicVideoJukebox.Core.ViewModels
             MetadataEntries.Clear();
             foreach (var entry in metadata)
             {
-                MetadataEntries.Add(new MetadataEntryViewModel(entry));               
+                MetadataEntries.Add(new MetadataEntryViewModel(entry));
             }
-        }
-
-        private DelegateCommand CreateSafeAsyncCommand(Func<Task> execute, Func<bool> canExecute = null!)
-        {
-            return new DelegateCommand(async () =>
-            {
-                try
-                {
-                    await execute();
-                }
-                catch (Exception ex)
-                {
-                    dialogService.ShowError($"Could not save: {ex.Message}");
-                    throw;
-                }
-            },
-            canExecute ?? (() => true)
-            );
         }
     }
 }
